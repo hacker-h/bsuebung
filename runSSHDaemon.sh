9801bd090e689e0b9baacf340d824d3de742caf6
Henning Häcker <henning.haecker@web.de>
2019-01-17 20:44:40 +0100

    extracted interactive shell command back to hard coded docker command

--- runSSHDaemon.sh
+++ runSSHDaemon.sh
@@ -5,7 +5,7 @@ HOST_DIR="/c/Users/Hacker/myhostdirectory" #path (Linux format!) to host directo
 CONTAINER_DIR="/mycontainerdirectory" #path (Linux format!) to the shared directory inside of the container
 CONTAINER_NAME="myfs-container" #name of the launched docker container
 IMAGE_NAME="myfs" #name of the docker image to build and use
-RUN_COMMANDS="cp -r /MyFS/* ${CONTAINER_DIR}; cd ${CONTAINER_DIR}; ls obj; make; bash" #commands to run after starting the container additional to the ssh daemon ('bash' starts an interactive shell)
+RUN_COMMANDS="cp -r /MyFS/* ${CONTAINER_DIR}; cd ${CONTAINER_DIR}; ls obj; make" #commands to run inside the container after starting the ssh daemon and before starting the interactive shell (bash)
 
 echo "Building Docker Image '${IMAGE_NAME}':"
 docker build -t "${IMAGE_NAME}" .
@@ -17,4 +17,4 @@ SSH Daemon will be running and listening on Port ${SSH_PORT}.
 User:     root
 Password: root
 "
-docker run -it --rm -v "/${HOST_DIR}:/${CONTAINER_DIR}" -v "obj:/MyFS/obj" --name "${CONTAINER_NAME}" -p "${SSH_PORT}":22 "${IMAGE_NAME}" bash -c "/usr/sbin/sshd; ${RUN_COMMANDS}"
+docker run -it --rm -v "/${HOST_DIR}:/${CONTAINER_DIR}" -v "obj:/MyFS/obj" --name "${CONTAINER_NAME}" -p "${SSH_PORT}":22 "${IMAGE_NAME}" bash -c "/usr/sbin/sshd; ${RUN_COMMANDS}; bash" #'bash' starts an interactive shell
